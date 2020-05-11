5d69a7c205a86fb1291c465810afc92452888dfe
Eduardo Rico <emrs94@gmail.com>
2020-05-11 14:52:21 -0500

    Add files via upload

---
+++ build/precache-manifest.dac6ed79f1ad965665260eedc2233cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68449550b9ff134885dc2fcc8d8e87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aada519a41d46de221ea",
+    "url": "/static/css/main.ac2918a9.chunk.css"
+  },
+  {
+    "revision": "b2398bba872c6380f288",
+    "url": "/static/js/2.a5b4943a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5b4943a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aada519a41d46de221ea",
+    "url": "/static/js/main.b622bd24.chunk.js"
+  },
+  {
+    "revision": "60d5a8b46fdc77f4b04c",
+    "url": "/static/js/runtime-main.410b4154.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
+
+function App() {
+  //definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState("");
+  //state para guardar las noticias
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=9c5ea783568d43d9b40900e92f82d4c6`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  return (
+    <Fragment>
+      <Header titulo='Buscador de noticias' />
+      <div className='container white'>
+        <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
